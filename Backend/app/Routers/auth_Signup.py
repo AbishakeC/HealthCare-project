d3b245554c5deb8a29728206979d460e044eb6ff
Abishake C <abishakec17a@hmail.com>
2026-03-13 21:44:28 +0530

    gitignore file creation

--- Backend/app/Routers/auth_Signup.py
+++ Backend/app/Routers/auth_Signup.py
@@ -1,61 +1,68 @@
-from fastapi import APIRouter,Depends,HTTPException
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from jose import jwt
-from ..Models.model import UserCreate,UserLogin
-
 
 from ..Configs.database import sessionlocal
 from ..Models import model
-from ..Middleware.hashing import hashedpassword
-from ..Middleware.hashing import verifypassword
+from ..Models.model import UserCreate, UserLogin
+from ..Middleware.hashing import hashedpassword, verifypassword
 from ..Middleware.token import create_access_token
 
 router = APIRouter()
+
 
 def get_db():
     db = sessionlocal()
     try:
         yield db
     finally:
-        db.close()  
+        db.close()
+
+
+# ---------------- SIGNUP ----------------
 
 @router.post("/Signup")
-def signup(user:UserCreate,db:Session = Depends(get_db)):
+def signup(user: UserCreate, db: Session = Depends(get_db)):
+
+    existing_user = db.query(model.User).filter(
+        model.User.email == user.email
+    ).first()
+
+    if existing_user:
+        raise HTTPException(400, "Email already registered")
 
     new_user = model.User(
         username=user.username,
-        email = user.email,
-        password_hashed = hashedpassword(user.password)
+        email=user.email,
+        password_hashed=hashedpassword(user.password)
     )
 
     db.add(new_user)
     db.commit()
     db.refresh(new_user)
 
-    return {"message":"user created with hashed password"}
+    return {"message": "User created successfully"}
 
 
+# ---------------- LOGIN ----------------
 
 @router.post("/Login")
-def login(user:UserLogin,db:Session=Depends(get_db)):
+def login(user: UserLogin, db: Session = Depends(get_db)):
+
     user_db = db.query(model.User).filter(
         model.User.email == user.email
     ).first()
 
     if not user_db:
-        raise HTTPException(401,"Invalid Credentials")
-    
-    if not verifypassword(user.password,user_db.password_hashed):
-        raise HTTPException(401,"invalid credential password")
-    
+        raise HTTPException(401, "Invalid credentials")
+
+    if not verifypassword(user.password, user_db.password_hashed):
+        raise HTTPException(401, "Invalid password")
 
     token = create_access_token(
-        {"user_id":user_db.id}
+        {"user_id": user_db.id}
     )
 
     return {
-        "access_token":token,
-        "token_type:":"Bearer"
+        "access_token": token,
+        "token_type": "Bearer"
     }
-
-
